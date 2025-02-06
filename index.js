5bb1c45db6e1511e85092a2e6b33d894d28a9697
ishathameemaa <ishathameemaa@gmail.com>
2025-02-06 08:31:27 +0530

    hhhh

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
+
 const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 const adminRoutes = require("./routes/admin.route.js");
